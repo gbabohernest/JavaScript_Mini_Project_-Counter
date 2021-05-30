0769962a2c88005a1a5802f1da9508944183b108
Gbaboh122 <73718883+Gbaboh122@users.noreply.github.com>
2021-05-31 00:21:38 +0300

    Final commit. Project done

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 // set initial count value
 let count = 0;
+
+// select the value & btns
+const countValue = document.querySelector("#value");
+const buttons = document.querySelectorAll(".btn");
+// console.log(buttons);
+
+// forEach to select all btns
+buttons.forEach(function (button) {
+  // console.log(button);
+
+  // add eventListener on each btn & use the event(e)oject
+  button.addEventListener("click", function (event) {
+    const btnClass = event.currentTarget.classList;
+
+    if (btnClass.contains("decrease")) {
+      count--;
+    } else if (btnClass.contains("reset")) {
+      count = 0;
+    } else {
+      count++;
+    }
+    // changing the colors for the counter
+    if (count < 0) {
+      countValue.style.color = "red";
+    }
+    if (count === 0) {
+      countValue.style.color = "#222";
+    }
+    if (count > 0) {
+      countValue.style.color = "green";
+    }
+    countValue.textContent = count;
+  });
+});
